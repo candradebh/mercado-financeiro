c6f8d2e09c451aa0e15f2e447858a95081be185b
Carlos Martins <carlos.martins@engdb.com.br>
2022-11-01 18:04:51 -0300

    nao adicionar via command

--- src/main/java/dev/carlosandrade/microservice/mercadofinanceiro/MercadoFinanceiroApplication.java
+++ src/main/java/dev/carlosandrade/microservice/mercadofinanceiro/MercadoFinanceiroApplication.java
@@ -1,6 +1,5 @@
 package dev.carlosandrade.microservice.mercadofinanceiro;
 
-import dev.carlosandrade.microservice.mercadofinanceiro.entity.Ativo;
 import dev.carlosandrade.microservice.mercadofinanceiro.repository.AtivoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,7 +11,7 @@ public class MercadoFinanceiroApplication implements CommandLineRunner {
 
 	@Autowired
 	AtivoRepository ativoRepository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(MercadoFinanceiroApplication.class, args);
 	}
@@ -22,13 +21,13 @@ public class MercadoFinanceiroApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 
-		Ativo ativo1 = Ativo.builder().ticker("VALE3").nome("VALE ON").tipoAtivo("Ações").build();
+		/*Ativo ativo1 = Ativo.builder().ticker("VALE3").nome("VALE ON").tipoAtivo("Ações").build();
 		Ativo ativo2 = Ativo.builder().ticker("PETR3").nome("PETROBRAS ON").tipoAtivo("Ações").build();
 		Ativo ativo3 = Ativo.builder().ticker("PETR4").nome("PETROBRAS PN").tipoAtivo("Ações").build();
 
 		ativoRepository.save(ativo1);
 		ativoRepository.save(ativo2);
-		ativoRepository.save(ativo3);
+		ativoRepository.save(ativo3);*/
 
 	}
 
