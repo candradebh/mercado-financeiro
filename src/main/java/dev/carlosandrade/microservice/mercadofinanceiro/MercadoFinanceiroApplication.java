a0a46841cca03c6ff221a253c58cf35faa62f7e7
Carlos Martins <carlos.martins@engdb.com.br>
2022-11-01 11:16:04 -0300

    primeiro comit

---
+++ src/main/java/dev/carlosandrade/microservice/mercadofinanceiro/MercadoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package dev.carlosandrade.microservice.mercadofinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/carlosandrade/microservice/mercadofinanceiro/MercadoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlosandrade.microservice.mercadofinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
