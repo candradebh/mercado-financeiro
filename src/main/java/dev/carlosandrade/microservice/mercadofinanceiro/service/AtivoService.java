79e83c0c01b0ae1cfbdb3f6a3dff1bfb08f12bb4
Carlos Andrade <dev_carlos@outlook.com>
2022-11-07 15:47:38 -0300

    trabalhando com service

---
+++ src/main/java/dev/carlosandrade/microservice/mercadofinanceiro/service/AtivoService.java
@@ -0,0 +1,77 @@
+package dev.carlosandrade.microservice.mercadofinanceiro.service;
+
+import dev.carlosandrade.microservice.mercadofinanceiro.controllers.AtivosController;
+import dev.carlosandrade.microservice.mercadofinanceiro.entity.Ativo;
+import dev.carlosandrade.microservice.mercadofinanceiro.repository.AtivoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class AtivoService {
+
+    @Autowired
+    AtivoRepository repository;
+
+
+    public List<Ativo> getAll() {
+
+        List<Ativo> listRegisters = repository.findAll();
+
+        if (!listRegisters.isEmpty()) {
+
+            for (Ativo register : listRegisters) {
+                String ticker = register.getTicker();
+                register.add(linkTo(methodOn(AtivosController.class).read(ticker)).withRel("read"));
+            }
+        }
+
+        return listRegisters;
+    }
+
+    public Ativo update(String ticker, Ativo ativo) {
+
+        Optional<Ativo> ativoExisting = repository.findByTicker(ticker);
+        if (ativoExisting.isPresent()) {
+            Ativo updatedAtivo = ativoExisting.get();
+
+            if (ativo.getNome() != null && !ativo.getNome().equals(updatedAtivo.getNome())) {
+                updatedAtivo.setNome(ativo.getNome());
+            }
+
+            if (ativo.getCnpj() != null && !ativo.getCnpj().equals(updatedAtivo.getCnpj())) {
+                updatedAtivo.setCnpj(ativo.getCnpj());
+            }
+
+            return (Ativo) updatedAtivo.add(linkTo(methodOn(AtivosController.class).getAll()).withRel("getAll"));
+
+        } else {
+            return null;
+        }
+    }
+
+    public Ativo read(String ticker) {
+
+        Optional<Ativo> register = repository.findByTicker(ticker);
+        return register.isPresent() ? (Ativo) register.get().add(linkTo(methodOn(AtivosController.class).getAll()).withRel("getAll")) : null;
+    }
+
+    public Ativo create(Ativo ativo) {
+
+        Optional<Ativo> ativoBuscado = repository.findByTicker(ativo.getTicker());
+        return !ativoBuscado.isPresent() ? (Ativo) repository.save(ativo).add(linkTo(methodOn(AtivosController.class).getAll()).withRel("getAll")) : null;
+    }
+
+    public boolean delete(String ticker) {
+
+        return repository.findByTicker(ticker).map(existingAtivo -> {
+            repository.delete(existingAtivo);
+            return true;
+        }).orElse(false);
+    }
+}
